09292fcfc6a6eb7fbb75c14efe78983ea80b90dc
許瑋軒 <3B232020@gm.student.ncut.edu.tw>
2026-01-02 01:51:22 +0800

    新增products路由，和說明

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,55 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::resource('products',ProductController::class);
+/*
+products.index:
+    URI:products
+    HTTP request的方法:GET
+    Controller或Method: ProductController@index
+    說明:列出所有products裡的資料
+products.show
+    URI:products/{product}
+    HTTP request的方法:GET
+    Controller或Method: ProductController@show
+    說明:顯示某項products的資料
+
+products.create 
+    URI:products/create
+    HTTP request的方法:GET
+    Controller或Method: ProductController@create
+    說明:顯示新增products的表單頁面。
+
+products.store 
+    URI:products
+    HTTP request的方法:POST
+    Controller或Method:ProductController@store
+    說明:儲存新增的products資料
+
+products.edit 
+    URI:products/{product}/edit
+    HTTP request的方法:GET
+    Controller或Method: ProductController@edit
+    說明:顯示編輯products的表單資料
+
+products.update 
+    URI:products/{product}
+    HTTP request的方法:PUT / PATCH
+    Controller或Method: ProductController@update
+    說明:更新某一項products的資料
+
+products.destroy
+    URI:products/{product}
+    HTTP request的方法:DELETE
+    Controller或Method: ProductController@destroy
+    說明:刪除某項products的資料
+*/
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
